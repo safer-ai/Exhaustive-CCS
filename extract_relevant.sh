a7684dfe2d32b17dba77a39da3251e927478e411
Fabien Roger <efab.roger@gmail.com>
2023-02-16 22:07:23 +0000

    Clean scripts

--- extract_relevant.sh
+++ extract_relevant.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
 # Extract UQA on datasets where it is good
-for seed in {0..4}; do
-    python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list CCS --seed $seed
+for seed in {1..4}; do
+    python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list CCS LR Random --seed $seed
     # copy the csv file so that it doesn't get overwritten
-    cp unifiedqa-t5-11b_normal_${seed}.csv uqa_good_${seed}.csv
+    cp extraction_results/unifiedqa-t5-11b_normal_${seed}.csv extraction_results/uqa_good_${seed}.csv
+done
+
+# Extract UQA states (once)
+python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list CCS LR Random --save_states --seed 0
+cp extraction_results/unifiedqa-t5-11b_normal_0.csv extraction_results/uqa_good_0.csv
+
+# Extract GPT-J on datasets where it is good
+for seed in {0..9}; do
+    python extraction_main.py --model gpt-j-6B --datasets imdb amazon-polarity ag-news dbpedia-14 --method_list CCS LR Random --seed $seed
+    # copy the csv file so that it doesn't get overwritten
+    cp extraction_results/gpt-j-6B_normal_${seed}.csv extraction_results/gptj_good_${seed}.csv
 done
