c45130163079a816e5e63eb1d138da6a79c445cb
Fabien Roger <efab.roger@gmail.com>
2023-02-17 09:32:23 +0000

    Rename test_on_train folder

--- extract_relevant.sh
+++ extract_relevant.sh
@@ -24,18 +24,18 @@ done
 
 # Extract UQA on datasets where it is good
 for seed in {1..4}; do
-    python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list CCS LR Random --seed $seed --save_dir extraction_results_train --test_on_train
+    python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list CCS LR Random --seed $seed --save_dir extraction_results/train --test_on_train
     # copy the csv file so that it doesn't get overwritten
-    cp extraction_results_train/unifiedqa-t5-11b_normal_${seed}.csv extraction_results_train/uqa_good_${seed}.csv
+    cp extraction_results/train/unifiedqa-t5-11b_normal_${seed}.csv extraction_results/train/uqa_good_${seed}.csv
 done
 
 # Extract UQA states (once)
-python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list CCS LR Random --save_states --seed 0 --save_dir extraction_results_train --test_on_train
-cp extraction_results_train/unifiedqa-t5-11b_normal_0.csv extraction_results_train/uqa_good_0.csv
+python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list CCS LR Random --save_states --seed 0 --save_dir extraction_results/train --test_on_train
+cp extraction_results/train/unifiedqa-t5-11b_normal_0.csv extraction_results/train/uqa_good_0.csv
 
 # Extract GPT-J on datasets where it is good
 for seed in {0..9}; do
-    python extraction_main.py --model gpt-j-6B --datasets imdb amazon-polarity ag-news dbpedia-14 --method_list CCS LR Random --seed $seed --save_dir extraction_results_train --test_on_train
+    python extraction_main.py --model gpt-j-6B --datasets imdb amazon-polarity ag-news dbpedia-14 --method_list CCS LR Random --seed $seed --save_dir extraction_results/train --test_on_train
     # copy the csv file so that it doesn't get overwritten
-    cp extraction_results_train/gpt-j-6B_normal_${seed}.csv extraction_results_train/gptj_good_${seed}.csv
+    cp extraction_results/train/gpt-j-6B_normal_${seed}.csv extraction_results/train/gptj_good_${seed}.csv
 done
