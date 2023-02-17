431f5c583fbd761289fa385a1d81dccd5150e439
Fabien Roger <efab.roger@gmail.com>
2023-02-17 09:07:27 +0000

    Add back uqa to generate_all

--- generate_all.sh
+++ generate_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-models=(roberta-large-mnli gpt-j-6B t5-11b T0pp deberta-v2-xxlarge-mnli)
+models=(gpt-j-6B unifiedqa-t5-11b roberta-large-mnli t5-11b T0pp deberta-v2-xxlarge-mnli)
 for model in "${models[@]}"; do
     python generation_main.py --model $model --datasets all --cal_zeroshot 0 --swipe
 done
