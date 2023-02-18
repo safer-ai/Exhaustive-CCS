23f2ef766518ecae8d2f5c2494d75e8e371fea39
Fabien Roger <efab.roger@gmail.com>
2023-02-18 21:30:32 +0000

    Add plotting for test on train

--- plots.py
+++ plots.py
@@ -6,14 +6,24 @@ import pandas as pd
 from pathlib import Path
 from typing import Literal, Optional
 
+# increase figure size to (8, 8)
+from matplotlib import rcParams
+
+rcParams["figure.figsize"] = (8, 8)
 # %%
 # Constants
 save_dir = Path("extraction_results")
+test_on_train = False
 UQA = "unifiedqa-t5-11b"
 GPTJ = "gpt-j-6b"
 Method = Literal["CCS", "LR", "Random"]
 uqa_good_datasets = ["imdb", "amazon-polarity", "ag-news", "dbpedia-14", "copa", "boolq", "story-cloze"]
 gptj_good_datasets = ["imdb", "amazon-polarity", "ag-news", "dbpedia-14"]
+
+# derived constants
+if test_on_train:
+    save_dir = save_dir / "test_on_train"
+test_on_train_suffix = "\n(On the train set)" if test_on_train else ""
 # %%
 # Utils
 
@@ -49,8 +59,10 @@ def load_stats(model_name: str, train: str, test: str, method: Method = "CCS"):
         "loss": np.array([df["loss"].mean() for df in dfs]),
     }
 
+
 def ccs_loss(p0, p1):
     return np.minimum(p0, p1).mean() + np.square(p0 + p1 - 1).mean()
+
 
 # %%
 # Plot 1.1 distribution of probs on copa
@@ -60,7 +72,7 @@ minp = np.minimum(p0, p1)
 maxp = np.maximum(p0, p1)
 plt.hist(minp, bins=30, alpha=0.5, label="min(p+,p-)", range=(0, 1))
 plt.hist(maxp, bins=30, alpha=0.5, label="max(p+,p-)", range=(0, 1))
-plt.title("Distribution of min(p+,p-) and max(p+,p-)\nCOPA, UQA, CCS")
+plt.title(f"Distribution of min(p+,p-) and max(p+,p-)\nCOPA, UQA, CCS{test_on_train_suffix}")
 plt.legend()
 plt.tight_layout()
 # %%
@@ -71,7 +83,7 @@ minp = np.minimum(p0, p1)
 maxp = np.maximum(p0, p1)
 plt.hist(minp, bins=30, alpha=0.5, label="min(p+,p-)", range=(0, 1))
 plt.hist(maxp, bins=30, alpha=0.5, label="max(p+,p-)", range=(0, 1))
-plt.title("Distribution of min(p+,p-) and max(p+,p-)\nAll datasets, UQA, CCS")
+plt.title(f"Distribution of min(p+,p-) and max(p+,p-)\nAll datasets, UQA, CCS{test_on_train_suffix}")
 plt.legend()
 plt.tight_layout()
 # %%
@@ -80,10 +92,12 @@ bar_names = uqa_good_datasets
 separate_losses = [load_stats(UQA, d, d, "CCS")["loss"] for d in bar_names]
 together_losses = [load_stats(UQA, "all", d, "CCS")["loss"] for d in bar_names]
 
+
 def get_mean_and_error(losses):
     means = np.mean(losses, axis=1)
     stds = np.std(losses, axis=1)
     return means, 2 * stds
+
 
 separate_mean, separate_error = get_mean_and_error(separate_losses)
 together_mean, together_error = get_mean_and_error(together_losses)
@@ -93,8 +107,10 @@ width = 0.35
 plt.bar(x_pos - width / 2, separate_mean, width, label="train separate", yerr=separate_error)
 plt.bar(x_pos + width / 2, together_mean, width, label="train together", yerr=together_error)
 plt.xticks(x_pos, bar_names, rotation=45)
-plt.title("Losses of UQA on all datasets\ntrained separately vs. trained together\n(2 std error bars over 5 runs)")
 plt.axhline(0.2, color="black", label="loss for constant guess")
+plt.title(
+    f"Losses of UQA on all datasets\ntrained separately vs. trained together\n(2 std error bars over 5 runs){test_on_train_suffix}"
+)
 plt.legend()
 plt.tight_layout()
 # %%
@@ -105,10 +121,12 @@ separate_accs = [load_stats(UQA, d, d, "CCS")["accuracy"] for d in bar_names]
 together_accs = [load_stats(UQA, "all", d, "CCS")["accuracy"] for d in bar_names]
 random_accs = [load_stats(UQA, d, d, "Random")["accuracy"] for d in bar_names]
 
+
 def get_mean_and_error(accs):
     means = np.mean(accs, axis=1)
     stds = np.std(accs, axis=1)
     return means, 2 * stds
+
 
 lr_mean, lr_error = get_mean_and_error(lr_accs)
 separate_mean, separate_error = get_mean_and_error(separate_accs)
@@ -127,8 +145,10 @@ plt.errorbar(x_pos - width / 2, separate_mean, label="CCS", yerr=separate_error,
 plt.errorbar(x_pos + width / 2, together_mean, label="CCS, trained together", yerr=together_error, **kwargs)
 plt.errorbar(x_pos + 3 * width / 2, random_mean, label="Random", yerr=random_error, **kwargs)
 plt.xticks(x_pos, bar_names, rotation=45)
-plt.title("Accuracies of UQA on all datasets\ntrained separately vs. trained together vs random directions\n(2 std error bars over 5 runs)")
-plt.ylim(0.5,1)
+plt.ylim(0.5, 1)
+plt.title(
+    f"Accuracies of UQA on all datasets\ntrained separately vs. trained together vs random directions\n(2 std error bars over 5 runs){test_on_train_suffix}"
+)
 plt.legend()
 plt.tight_layout()
 # %%
@@ -139,7 +159,7 @@ minp = np.minimum(p, 1 - p)
 maxp = np.maximum(p, 1 - p)
 plt.hist(minp, bins=30, alpha=0.5, label="min(p,1-p)", range=(0, 1))
 plt.hist(maxp, bins=30, alpha=0.5, label="max(p,1-p)", range=(0, 1))
-plt.title("Distribution of min(p+,p-) and max(p+,p-)\nAll datasets, UQA, LR")
+plt.title(f"Distribution of min(p+,p-) and max(p+,p-)\nAll datasets, UQA, LR{test_on_train_suffix}")
 plt.legend()
 plt.tight_layout()
 # %%
@@ -150,7 +170,7 @@ minp = np.minimum(p0, p1)
 maxp = np.maximum(p0, p1)
 plt.hist(minp, bins=30, alpha=0.5, label="min(p+,p-)", range=(0, 1))
 plt.hist(maxp, bins=30, alpha=0.5, label="max(p+,p-)", range=(0, 1))
-plt.title("Distribution of min(p+,p-) and max(p+,p-)\nAll datasets, UQA, CCS")
+plt.title(f"Distribution of min(p+,p-) and max(p+,p-)\nAll datasets, UQA, CCS{test_on_train_suffix}")
 plt.legend()
 plt.tight_layout()
 # %%
@@ -167,10 +187,12 @@ bar_names = gptj_good_datasets
 separate_losses = [load_stats(GPTJ, d, d, "CCS")["loss"] for d in bar_names]
 together_losses = [load_stats(GPTJ, "all", d, "CCS")["loss"] for d in bar_names]
 
+
 def get_mean_and_error(losses):
     means = np.mean(losses, axis=1)
     stds = np.std(losses, axis=1)
     return means, 2 * stds
+
 
 separate_mean, separate_error = get_mean_and_error(separate_losses)
 together_mean, together_error = get_mean_and_error(together_losses)
@@ -180,8 +202,10 @@ width = 0.35
 plt.bar(x_pos - width / 2, separate_mean, width, label="train separate", yerr=separate_error)
 plt.bar(x_pos + width / 2, together_mean, width, label="train together", yerr=together_error)
 plt.xticks(x_pos, bar_names, rotation=45)
-plt.title("Losses of GPTJ on all datasets\ntrained separately vs. trained together\n(2 std error bars over 10 runs)")
 plt.axhline(0.2, color="black", label="loss for constant guess")
+plt.title(
+    f"Losses of GPTJ on all datasets\ntrained separately vs. trained together\n(2 std error bars over 10 runs){test_on_train_suffix}"
+)
 plt.legend()
 plt.tight_layout()
 # %%
@@ -192,10 +216,12 @@ separate_accs = [load_stats(GPTJ, d, d, "CCS")["accuracy"] for d in bar_names]
 together_accs = [load_stats(GPTJ, "all", d, "CCS")["accuracy"] for d in bar_names]
 random_accs = [load_stats(GPTJ, d, d, "Random")["accuracy"] for d in bar_names]
 
+
 def get_mean_and_error(accs):
     means = np.mean(accs, axis=1)
     stds = np.std(accs, axis=1)
     return means, 2 * stds
+
 
 lr_mean, lr_error = get_mean_and_error(lr_accs)
 separate_mean, separate_error = get_mean_and_error(separate_accs)
@@ -214,8 +240,10 @@ plt.errorbar(x_pos - width / 2, separate_mean, label="CCS", yerr=separate_error,
 plt.errorbar(x_pos + width / 2, together_mean, label="CCS, trained together", yerr=together_error, **kwargs)
 plt.errorbar(x_pos + 3 * width / 2, random_mean, label="Random", yerr=random_error, **kwargs)
 plt.xticks(x_pos, bar_names, rotation=45)
-plt.title("Accuracies of GPTJ on all datasets\ntrained separately vs. trained together vs random directions\n(2 std error bars over 10 runs)")
-plt.ylim(0.5,1)
+plt.ylim(0.5, 1)
+plt.title(
+    f"Accuracies of GPTJ on all datasets\ntrained separately vs. trained together vs random directions\n(2 std error bars over 10 runs){test_on_train_suffix}"
+)
 plt.legend()
 plt.tight_layout()
 # %%
