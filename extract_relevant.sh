ad4d4925d49d55a2e2a1e3740ff6e949748c81e9
Fabien Roger <efab.roger@gmail.com>
2023-02-20 22:34:17 +0000

    Fix test_on_train dir

--- extract_relevant.sh
+++ extract_relevant.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+methods="CCS LR Random CCS-md LR-md Random-md"
+uqa_good_ds="imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze"
+gptj_good_ds="imdb amazon-polarity ag-news dbpedia-14"
 
 # Extract UQA states (once)
-python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list CCS LR Random  --save_states --seed 0
+python extraction_main.py --model unifiedqa-t5-11b --datasets $uqa_good_ds --method_list $methods  --save_states --seed 0
 cp extraction_results/unifiedqa-t5-11b_normal_0.csv extraction_results/uqa_good_0.csv
 
 # Extract UQA on datasets where it is good
 for seed in {1..9}; do
-    python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list CCS LR Random --seed $seed
+    python extraction_main.py --model unifiedqa-t5-11b --datasets $uqa_good_ds --method_list $methods --seed $seed
     # copy the csv file so that it doesn't get overwritten
     cp extraction_results/unifiedqa-t5-11b_normal_${seed}.csv extraction_results/uqa_good_${seed}.csv
 done
 
 # Extract GPT-J on datasets where it is good
 for seed in {0..9}; do
-    python extraction_main.py --model gpt-j-6B --datasets imdb amazon-polarity ag-news dbpedia-14 --method_list CCS LR Random --seed $seed
+    python extraction_main.py --model gpt-j-6B --datasets $gptj_good_ds --method_list $methods --seed $seed
     # copy the csv file so that it doesn't get overwritten
     cp extraction_results/gpt-j-6B_normal_${seed}.csv extraction_results/gptj_good_${seed}.csv
 done
@@ -23,21 +26,21 @@ done
 # Same but test on train
 
 # Extract UQA states (once)
-python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list CCS LR Random --save_states --seed 0 --save_dir extraction_results/train --test_on_train
-cp extraction_results/train/unifiedqa-t5-11b_normal_0.csv extraction_results/train/uqa_good_0.csv
+python extraction_main.py --model unifiedqa-t5-11b --datasets $uqa_good_ds --method_list $methods --save_states --seed 0 --save_dir extraction_results/train --test_on_train
+cp extraction_results/test_on_train/unifiedqa-t5-11b_normal_0.csv extraction_results/test_on_train/uqa_good_0.csv
 
 # Extract UQA on datasets where it is good
 for seed in {1..4}; do
-    python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list CCS LR Random --seed $seed --save_dir extraction_results/train --test_on_train
+    python extraction_main.py --model unifiedqa-t5-11b --datasets $uqa_good_ds --method_list $methods --seed $seed --save_dir extraction_results/train --test_on_train
     # copy the csv file so that it doesn't get overwritten
-    cp extraction_results/train/unifiedqa-t5-11b_normal_${seed}.csv extraction_results/train/uqa_good_${seed}.csv
+    cp extraction_results/test_on_train/unifiedqa-t5-11b_normal_${seed}.csv extraction_results/test_on_train/uqa_good_${seed}.csv
 done
 
 # Extract GPT-J on datasets where it is good
 for seed in {0..9}; do
-    python extraction_main.py --model gpt-j-6B --datasets imdb amazon-polarity ag-news dbpedia-14 --method_list CCS LR Random --seed $seed --save_dir extraction_results/train --test_on_train
+    python extraction_main.py --model gpt-j-6B --datasets $gptj_good_ds --method_list $methods --seed $seed --save_dir extraction_results/train --test_on_train
     # copy the csv file so that it doesn't get overwritten
-    cp extraction_results/train/gpt-j-6B_normal_${seed}.csv extraction_results/train/gptj_good_${seed}.csv
+    cp extraction_results/test_on_train/gpt-j-6B_normal_${seed}.csv extraction_results/test_on_train/gptj_good_${seed}.csv
 done
 
 # RRCS on UQA
@@ -45,12 +48,12 @@ done
 RCCS_STRING=$(printf "RCCS%s " $(seq 0 19))
 
 # Extract UQA states (once)
-python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list $RCCS_STRING  --save_states --seed 0
+python extraction_main.py --model unifiedqa-t5-11b --datasets $uqa_good_ds --method_list $RCCS_STRING  --save_states --seed 0
 cp extraction_results/unifiedqa-t5-11b_normal_0.csv extraction_results/uqa_goodrccs_0.csv
 
 # Extract UQA on datasets where it is good
 for seed in {1..9}; do
-    python extraction_main.py --model unifiedqa-t5-11b --datasets imdb amazon-polarity ag-news dbpedia-14 copa boolq story-cloze --method_list $RCCS_STRING --seed $seed
+    python extraction_main.py --model unifiedqa-t5-11b --datasets $uqa_good_ds --method_list $RCCS_STRING --seed $seed
     # copy the csv file so that it doesn't get overwritten
     cp extraction_results/unifiedqa-t5-11b_normal_${seed}.csv extraction_results/uqa_goodrccs_${seed}.csv
 done
